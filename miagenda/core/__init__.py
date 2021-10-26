80d987af4349b64548c609b7e6b462367ea45b06
Joules <joules.hdz@gmail.com>
2021-10-26 00:14:05 -0500

    :card_file_box: Database prod url

--- miagenda/core/__init__.py
+++ miagenda/core/__init__.py
@@ -2,7 +2,12 @@ from flask import Flask
 import os
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if 'postgresql' not in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace('postgres', 'postgresql')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
 from Agendas import views
 from . import database
