9fbf50fbf143f8a8edd9ccb41269f02a63133a02
Joules <joules.hdz@gmail.com>
2021-10-25 23:58:18 -0500

    :rocket: Migrate Folder

--- miagenda/manage.py
+++ miagenda/manage.py
@@ -2,9 +2,10 @@ from core import app, database
 #from core.database import db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+import os
 
 
-migrate = Migrate(app, database.db, directory='migrations_joules/')
+migrate = Migrate(app, database.db, directory=os.getenv('MIGRATE_FOLDER', 'migrations_joules/'))
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
